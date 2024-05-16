e73a06d393a953a0d8da3f3c6f5b5902ea7eb06d
Jort Driegen <39172426+jortdr@users.noreply.github.com>
2024-05-16 10:46:33 +0200

    Improved Prompt response for json cast - forcing materials

--- app/SystemPrompts/Prompts.php
+++ app/SystemPrompts/Prompts.php
@@ -8,6 +8,9 @@ class Prompts
     {
         return
             'Parse all the given sentences into this JSON format. If you are unable to parse certain fields, set the value to null.
+            The amounts should be clamped between 0 and 10. If the amount is beyond this range, set it to the nearest valid value.
+            The following items are valid for RequestedItem and OfferedItem. Try to match the item as closely as possible to one of these:
+            Wood, Lenses, Clay, Gold, Steel, Insulation, Fertilizer, Stone
         {
             "TargetName": "(Azari/Beluga/Cinatu)",
             "RequestedItem": "Item Name",
@@ -19,18 +22,17 @@ class Prompts
 
     public static function dealSystemPrompt(): string
     {
-        return 'You are a human player in a negotiation game responding to another player. Do not use any special characters in your response.';
+        return 'You are a human player in a negotiation game responding to another player. Do not use special characters or emoji in your response.';
     }
 
     public static function acceptDealPrompt(array $dealData): string
     {
         //TODO: Implement the tribe name and reason for accepting the deal
         $tribe = "Azari";
-        $reason = "come up with a reason for accepting the deal here.";
         return
             $tribe . ' requested ' . $dealData['RequestedAmount'] . ' ' . $dealData['RequestedItem'] . ' in exchange for your ' . $dealData['OfferedAmount'] . ' ' . $dealData['OfferedItem'] .
             'Write a response to accept the following deal from the ' . $tribe . ' tribe leader. Use a ' . $dealData['speakerStyle'] . ' tone in your response.
-            You are accepting the deal for the following reason: ' . $reason . '. Include this reason in your response.';
+            You are accepting the deal for the following reason: ' . $dealData['reason'] . '. Include this reason in your response.';
     }
 
     public static function rejectDealPrompt(array $dealData): string
